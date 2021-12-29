b1be70ada2b60ecc0c36485528e2d88531a09a72
s1mple <dimabelenov@gmail.com>
2021-12-30 01:40:59 +0200

    feature: ad dtest for select element

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import App from './App';
 
 describe('App component', () => {
@@ -38,4 +40,43 @@ describe('Events', () => {
 		search.focus();
 		expect(search).toHaveFocus();
 	});
+
 });
+
+
+
+describe('App component', () => {
+	it('UserEvent', async () => {
+		render(<App />);
+
+		expect(screen.queryByText('User')).toBeNull();
+		expect(await screen.findByText('User')).toBeInTheDocument();
+
+		const searchEl = screen.getByTestId('search');
+
+		userEvent.type(searchEl,'Hello');
+
+		expect(screen.queryByText('HelloUser')).toBeInTheDocument();
+    userEvent.clear(searchEl);
+		screen.debug()
+    expect(screen.queryByText('User')).toBeInTheDocument();
+	});
+});
+
+
+describe('Select', () => {
+  it('render select', () => {
+    const { getByTestId, getAllByTestId } = render(
+			<select data-testid="select">
+				<option data-testid="select-option" value="1">A</option>
+				<option data-testid="select-option" value="2">B</option>
+				<option data-testid="select-option" value="3">C</option>
+			</select>
+		);
+    const select = getByTestId('select');
+    userEvent.selectOptions(select,"1");
+    const options = getAllByTestId('select-option');
+    expect((options[0] as HTMLOptionElement).selected).toBeTruthy();
+    expect((options[1] as HTMLOptionElement).selected).toBeFalsy();
+  })
+})
